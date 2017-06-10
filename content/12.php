4913a0d2357cca1d1b8e16184b9e51cdc27345c1
KaZzik <kazzick@mail.ru>
2017-06-10 23:14:38 +0300

    Initial commit

---
+++ content/12.php
@@ -0,0 +1,304 @@
+<?php
+    //  вся процедура работает на сессиях. Именно в ней хранятся данные  пользователя, пока он находится на сайте. Очень важно запустить их в  самом начале странички!!!
+    session_start();
+    ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" lang="ru">
+<link rel="stylesheet" type="text/css" href="css\1.css">
+  <title>Тест по теме Редактирование и изменению обьектов</title>
+</head>
+<body >
+<div id="l1"><img src="images\autocad-u150-fr.png"></div>
+<div id="head"><p>Электронное учебное пособие по программе AutoCAD</p></div>
+<content><div><p>&nbsp;</p>
+      <div class="clearfix colelem" id="pu254753-51"><!-- group -->
+     <div class="rgba-background clearfix grpelem shared_content" id="u254753-51" data-content-guid="u254753-51_content"><!-- content -->
+   <p align="center"><b>Тест по теме «Редактирование объектов»</b></p>
+     <p><script type="text/javascript" language="JavaScript">
+ var res="22144131423313421124" 
+function check_me()
+{
+    var count=0
+    with(document.test)
+{
+if (!Q1[0].checked&&!Q1[1].checked&&!Q1[2].checked&&!Q1[3].checked&&!Q1[4].checked)  
+{count+=1};  
+if (!Q2[0].checked&&!Q2[1].checked&&!Q2[2].checked&&!Q2[3].checked&&!Q2[4].checked)  
+{count+=1};  
+if (!Q3[0].checked&&!Q3[1].checked&&!Q3[2].checked&&!Q3[3].checked&&!Q3[4].checked)  
+{count+=1};  
+if (!Q4[0].checked&&!Q4[1].checked&&!Q4[2].checked&&!Q4[3].checked&&!Q4[4].checked)  
+{count+=1};  
+if (!Q5[0].checked&&!Q5[1].checked&&!Q5[2].checked&&!Q5[3].checked&&!Q5[4].checked)  
+{count+=1};  
+if (!Q6[0].checked&&!Q6[1].checked&&!Q6[2].checked&&!Q6[3].checked&&!Q6[4].checked)  
+{count+=1};  
+if (!Q7[0].checked&&!Q7[1].checked&&!Q7[2].checked&&!Q7[3].checked&&!Q7[4].checked)  
+{count+=1};  
+if (!Q8[0].checked&&!Q8[1].checked&&!Q8[2].checked&&!Q8[3].checked&&!Q8[4].checked)  
+{count+=1};  
+if (!Q9[0].checked&&!Q9[1].checked&&!Q9[2].checked&&!Q9[3].checked&&!Q9[4].checked)  
+{count+=1};  
+if (!Q10[0].checked&&!Q10[1].checked&&!Q10[2].checked&&!Q10[3].checked&&!Q10[4].checked)  
+{count+=1};  
+if (!Q11[0].checked&&!Q11[1].checked&&!Q11[2].checked&&!Q11[3].checked&&!Q11[4].checked)  
+{count+=1};  
+if (!Q12[0].checked&&!Q12[1].checked&&!Q12[2].checked&&!Q12[3].checked&&!Q12[4].checked)  
+{count+=1};  
+if (!Q13[0].checked&&!Q13[1].checked&&!Q13[2].checked&&!Q13[3].checked&&!Q13[4].checked)  
+{count+=1};  
+if (!Q14[0].checked&&!Q14[1].checked&&!Q14[2].checked&&!Q14[3].checked&&!Q14[4].checked)  
+{count+=1};  
+if (!Q15[0].checked&&!Q15[1].checked&&!Q15[2].checked&&!Q15[3].checked&&!Q15[4].checked)  
+{count+=1};  
+if (!Q16[0].checked&&!Q16[1].checked&&!Q16[2].checked&&!Q16[3].checked&&!Q16[4].checked)  
+{count+=1};  
+if (!Q17[0].checked&&!Q17[1].checked&&!Q17[2].checked&&!Q17[3].checked&&!Q17[4].checked)  
+{count+=1};  
+if (!Q18[0].checked&&!Q18[1].checked&&!Q18[2].checked&&!Q18[3].checked&&!Q18[4].checked)  
+{count+=1};  
+if (!Q19[0].checked&&!Q19[1].checked&&!Q19[2].checked&&!Q19[3].checked&&!Q19[4].checked)  
+{count+=1};  
+if (!Q20[0].checked&&!Q20[1].checked&&!Q20[2].checked&&!Q20[3].checked&&!Q20[4].checked)  
+{count+=1};  
+if (count>0) 
+    {alert("Вы выполнили не все задания. Проверьте себя!")  } 
+        else answer()
+    }
+} 
+ 
+function control(k, f1,f2,f3,f4,f5,f6,f7,f8,f9,f10f11,f12,f13,f14,f15,f16,f17,f18,f19,f20) {
+if (k==1&&f1.checked) return true;
+if (k==2&&f2.checked) return true;
+if (k==3&&f3.checked) return true;
+if (k==4&&f4.checked) return true;
+if (k==5&&f5.checked) return true;
+if (k==6&&f6.checked) return true;
+if (k==7&&f7.checked) return true;
+if (k==8&&f8.checked) return true;
+if (k==9&&f9.checked) return true;
+if (k==10&&f10.checked) return true;
+if (k==11&&f11.checked) return true;
+if (k==12&&f12.checked) return true;
+if (k==13&&f13.checked) return true;
+if (k==14&&f14.checked) return true;
+if (k==15&&f15.checked) return true;
+if (k==16&&f16.checked) return true;
+if (k==17&&f17.checked) return true;
+if (k==18&&f18.checked) return true;
+if (k==19&&f19.checked) return true;
+if (k==20&&f20.checked) return true;
+return false;
+}
+ 
+function answer()
+{
+answ="";
+     with(document)
+    {
+    answ+=control(res.charAt(0) ,test.Q1[0],test.Q1[1],test.Q1[2],test.Q1[3],test.Q1[4])?"1":"0"
+answ+=control(res.charAt(1) ,test.Q2[0],test.Q2[1],test.Q2[2],test.Q2[3],test.Q2[4])?"1":"0"
+answ+=control(res.charAt(2) ,test.Q3[0],test.Q3[1],test.Q3[2],test.Q3[3],test.Q3[4])?"1":"0"
+answ+=control(res.charAt(3) ,test.Q4[0],test.Q4[1],test.Q4[2],test.Q4[3],test.Q4[4])?"1":"0"
+answ+=control(res.charAt(4) ,test.Q5[0],test.Q5[1],test.Q5[2],test.Q5[3],test.Q5[4])?"1":"0"
+answ+=control(res.charAt(5) ,test.Q6[0],test.Q6[1],test.Q6[2],test.Q6[3],test.Q6[4])?"1":"0"
+answ+=control(res.charAt(6) ,test.Q7[0],test.Q7[1],test.Q7[2],test.Q7[3],test.Q7[4])?"1":"0"
+answ+=control(res.charAt(7) ,test.Q8[0],test.Q8[1],test.Q8[2],test.Q8[3],test.Q8[4])?"1":"0"
+answ+=control(res.charAt(8) ,test.Q9[0],test.Q9[1],test.Q9[2],test.Q9[3],test.Q9[4])?"1":"0"
+answ+=control(res.charAt(9) ,test.Q10[0],test.Q10[1],test.Q10[2],test.Q10[3],test.Q10[4])?"1":"0"
+answ+=control(res.charAt(10) ,test.Q11[0],test.Q11[1],test.Q11[2],test.Q11[3],test.Q11[4])?"1":"0"
+answ+=control(res.charAt(11) ,test.Q12[0],test.Q12[1],test.Q12[2],test.Q12[3],test.Q12[4])?"1":"0"
+answ+=control(res.charAt(12) ,test.Q13[0],test.Q13[1],test.Q13[2],test.Q13[3],test.Q13[4])?"1":"0"
+answ+=control(res.charAt(13) ,test.Q14[0],test.Q14[1],test.Q14[2],test.Q14[3],test.Q14[4])?"1":"0"
+answ+=control(res.charAt(14) ,test.Q15[0],test.Q15[1],test.Q15[2],test.Q15[3],test.Q15[4])?"1":"0"
+answ+=control(res.charAt(15) ,test.Q16[0],test.Q16[1],test.Q16[2],test.Q16[3],test.Q16[4])?"1":"0"
+answ+=control(res.charAt(16) ,test.Q17[0],test.Q17[1],test.Q17[2],test.Q17[3],test.Q17[4])?"1":"0"
+answ+=control(res.charAt(17) ,test.Q18[0],test.Q18[1],test.Q18[2],test.Q18[3],test.Q18[4])?"1":"0"
+answ+=control(res.charAt(18) ,test.Q19[0],test.Q19[1],test.Q19[2],test.Q19[3],test.Q19[4])?"1":"0"
+answ+=control(res.charAt(19) ,test.Q20[0],test.Q20[1],test.Q20[2],test.Q20[3],test.Q20[4])?"1":"0"
+ 
+showResult();
+    }
+}
+ 
+function showResult()
+{
+    var nok=0;
+    var i,s;
+ 
+for (i=0; i<answ.length;i++) {nok+=answ.charAt(i)=="1"?1:0;}
+if(nok==20) s="10";
+if(nok<20) s="9";
+if(nok<18) s="8";
+if(nok<16) s="7";
+if(nok<14) s="6";
+if(nok<12) s="5";
+if(nok<10) s="4";
+if(nok<8) s="3";
+if(nok<6) s="2";
+if(nok<4) s="1";
+    document.test.s1.
+    value="Количество правильных ответов "+nok+". Ваша оценка  "+s+". Посмотрите на окно рядом с номером вопроса. Если ответ правильный, там (+). Если ответ ошибочен, там (-).";
+ 
+with(document.test)
+    {
+    if (answ.charAt(0)=="1") {T1.value=" + "} else {T1.value=" -"};
+   if (answ.charAt(1)=="1") {T2.value=" + "} else {T2.value=" - "};
+   if (answ.charAt(2)=="1") {T3.value=" + "} else {T3.value=" - "};
+   if (answ.charAt(3)=="1") {T4.value=" + "} else {T4.value=" - "};
+   if (answ.charAt(4)=="1") {T5.value=" + "} else {T5.value=" - "};
+   if (answ.charAt(5)=="1") {T6.value=" + "} else {T6.value=" - "};
+   if (answ.charAt(6)=="1") {T7.value=" + "} else {T7.value=" - "};
+   if (answ.charAt(7)=="1") {T8.value=" + "} else {T8.value=" - "};
+   if (answ.charAt(8)=="1") {T9.value=" + "} else {T9.value=" - "};
+   if (answ.charAt(9)=="1") {T10.value=" + "} else {T10.value=" - "};
+   if (answ.charAt(10)=="1") {T11.value=" + "} else {T11.value=" - "};
+   if (answ.charAt(11)=="1") {T12.value=" + "} else {T12.value=" - "};
+   if (answ.charAt(12)=="1") {T13.value=" + "} else {T13.value=" - "};
+   if (answ.charAt(13)=="1") {T14.value=" + "} else {T14.value=" - "};
+   if (answ.charAt(14)=="1") {T15.value=" + "} else {T15.value=" - "};
+   if (answ.charAt(15)=="1") {T16.value=" + "} else {T16.value=" - "};
+   if (answ.charAt(16)=="1") {T17.value=" + "} else {T17.value=" - "};
+   if (answ.charAt(17)=="1") {T18.value=" + "} else {T18.value=" - "};
+   if (answ.charAt(18)=="1") {T19.value=" + "} else {T19.value=" - "};  
+   if (answ.charAt(19)=="1") {T20.value=" + "} else {T20.value=" - "};
+     }
+}
+function showhide(obj){
+    if(obj == 'none') return 'inline';
+    else return 'none';
+}
+</script>
+<form name="test">
+<p align="CENTER"><b></b>
+  <ol>
+  <li><b>
+    <input type="text" size="1" name="T1"/>На какой панели инструментов расположены кнопки команд общего редактирования?</b><br/>
+<input type="radio" name="Q1"/> рисование;<br />
+<input type="radio" name="Q1"/> редактирование;<br />
+<input type="radio" name="Q1"/> стандартная;<br />
+<input type="radio" name="Q1"/> форматирование.<br />
+<b> </b><br/></li><li><INPUT type="text" size="1" name="T2"/><b>Команда, предназначенная для создания группы копий одних и тех же объектов, причем копии располагаются по определенному закону?</b><br/>
+<input type="radio" name="Q2"/> Сдвиг;<br />
+<input type="radio" name="Q2"/> Массив;<br />
+<input type="radio" name="Q2"/> Копировать;<br />
+<input type="radio" name="Q2"/> Зеркало.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T3"/>Команда, выполняющая операцию подрезки 2-х пересекающихся прямолинейных сегментов:</b><br/>
+<input type="radio" name="Q3"/> Фаска;<br />
+<input type="radio" name="Q3"/> Сопряжение;<br />
+<input type="radio" name="Q3"/> Разорвать;<br />
+<input type="radio" name="Q3"/> Обрезать.<br />
+<br/></li><li><INPUT type="text" size="1" name="T4"/><b>Что обозначает кнопка <img height="20" width="20" src="images\42.jpg"></b><br/>
+<input type="radio" name="Q4"/> Массив; <br />
+<input type="radio" name="Q4"/> Сдвиг;<br />
+<input type="radio" name="Q4"/> Повернуть;<br />
+<input type="radio" name="Q4"/> Масштаб.<br />
+<br/></li><li><INPUT type="text" size="1" name="T5"/><b>Команда общего редактирования «Переместить»</b><br/>
+<input type="radio" name="Q5"/> Copy;<br />
+<input type="radio" name="Q5"/> Mirror;<br />
+<input type="radio" name="Q5"/> Array;<br />
+<input type="radio" name="Q5"/> Move.<br />
+<br/></li><li><INPUT type="text" size="1" name="T6"/><b>Команда общего редактирования «Повернуть»</b><br/>
+<input type="radio" name="Q6"/> Rotate;<br />
+<input type="radio" name="Q6"/> Scale;<br />
+<input type="radio" name="Q6"/> Erase;<br />
+<input type="radio" name="Q6"/> Stretch.<br />
+<br/></li><li><INPUT type="text" size="1" name="T7"/><b>Команда общего редактирования «Фаска»</b><br/>
+<input type="radio" name="Q7"/> Trim;<br />
+<input type="radio" name="Q7"/> Extend;<br />
+<input type="radio" name="Q7"/> Chamfer;<br />
+<input type="radio" name="Q7"/> Fillet.<br />
+<br/></li><li><INPUT type="text" size="1" name="T8"/><b><img height="20" width="20" src="images\image001 (4).png">- кнопка какой команды?</b><br/>
+<input type="radio" name="Q8"/> Копировать;<br />
+<input type="radio" name="Q8"/> Обрезать;<br />
+<input type="radio" name="Q8"/> Масштабировать;<br />
+<input type="radio" name="Q8"/> Соединить.<br />
+<br/></li><li><INPUT type="text" size="1" name="T9"/><b><img height="20" width="20" src="images\27.jpg">- кнопка какой команды?</b><br/>
+<input type="radio" name="Q9"/> Зеркало;<br />
+<input type="radio" name="Q9"/> Разорвать;<br />
+<input type="radio" name="Q9"/> Сместить;<br />
+<input type="radio" name="Q9"/> Обрезать.<br />
+<br/></li><li><INPUT type="text" size="1" name="T10"/><b><img height="20" width="20" src="images\19.jpg">- кнопка какой команды?</b><br/>
+<input type="radio" name="Q10"/> Повторить;<br />
+<input type="radio" name="Q10"/> Повернуть;<br />
+<input type="radio" name="Q10"/> Сместить;<br />
+<input type="radio" name="Q10"/> Отменить.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T11"/>
+<img height="20" width="20" src="images\image001 (5).png">- кнопка, какой команды?</b><br/>
+<input type="radio" name="Q11"/> Фаска;<br />
+<input type="radio" name="Q11"/> Масштаб;<br />
+<input type="radio" name="Q11"/> Массив;<br />
+<input type="radio" name="Q11"/> Копировать.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T12"/>Какая команда осуществляет вычерчивание массива?</b><br/>
+<input type="radio" name="Q12"/> Polygon;<br />
+<input type="radio" name="Q12"/> Rectang;<br />
+<input type="radio" name="Q12"/> Array;<br />
+<input type="radio" name="Q12"/> Chamfer.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T13"/>Выберите правильную последовательность действий  для масштабирования объекта:</b><br/>
+<input type="radio" name="Q13"/> Выбрать инструмент Масштаб, выбрать объект для масштабирования, задать базовую точку, ввести коэффициент масштабирования;<br />
+<input type="radio" name="Q13"/> Выбрать инструмент Масштаб, ввести коэффициент масштабирования, задать базовую точку, выбрать объект для масштабирования;<br />
+<input type="radio" name="Q13"/> Выбрать инструмент Масштаб, ввести коэффициент масштабирования, выбрать объекты масштабирования;<br />
+<input type="radio" name="Q13"/> Выбрать инструмент Масштаб, выбрать базовую точку масштабирования, выбрать объект масштабирования.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T14"/>Массив, в котором объекты расставляются в нескольких строках и столбцах:</b><br/>
+<input type="radio" name="Q14"/> круговой;<br />
+<input type="radio" name="Q14"/> многоугольный;<br />
+<input type="radio" name="Q14"/> прямоугольный;<br />
+<input type="radio" name="Q14"/> квадратный.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T15"/>Массив, в котором объекты размножаются вдоль дуги окружности с некоторым центром и с заданным центральным углом:</b><br/>
+<input type="radio" name="Q15"/> прямоугольный;<br />
+<input type="radio" name="Q15"/> многоугольный;<br />
+<input type="radio" name="Q15"/> полукруговой;<br />
+<input type="radio" name="Q15"/> круговой.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T16"/>Какой режим AutoCAD позволяет включать режим ортогональности?</b><br/>
+<input type="radio" name="Q16"/> След;<br />
+<input type="radio" name="Q16"/> Орто;<br />
+<input type="radio" name="Q16"/> Поляр;<br />
+<input type="radio" name="Q16"/> Шаг.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T17"/>Выберите правильную последовательность действий  для обрезки объектов:</b><br/>
+<input type="radio" name="Q17"/> Взять инструмент Обрезка, нажать Enter, обрезать ненужные элементы, нажать Esc;<br />
+<input type="radio" name="Q17"/> Взять инструмент Обрезка, обрезать ненужные элементы, нажать Enter;<br />
+<input type="radio" name="Q17"/> Взять инструмент Обрезка, нажать Enter, указать расстояние смещения;<br />
+<input type="radio" name="Q17"/> Взять инструмент Обрезка, указать направление смещения, нажать Enter.<br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T18"/>Какую кнопку имеет команда Сопряжение?</b><br/>
+<input type="radio" name="Q18"/> <img height="20" width="20" src="images\29.jpg"><br />
+<input type="radio" name="Q18"/> <img height="20" width="20" src="images\6.jpg"><br />
+<input type="radio" name="Q18"/> <img height="20" width="20" src="images\9.jpg"><br />
+<input type="radio" name="Q18"/> <img height="20" width="20" src="images\10.jpg"><br />
+<input type="radio" name="Q18"/> <img height="20" width="20" src="images\2.jpg"><br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T19"/>Какую функцию выполняет команда Сопряжение (fillet)?</b><br/>
+<input type="radio" name="Q19"/> Обрезка;<br />
+<input type="radio" name="Q19"/> Плавное скругление;<br />
+<input type="radio" name="Q19"/> Соединение конечных отрезков;<br />
+<input type="radio" name="Q19"/> Построение сплайна. <br />
+<br/></li><li><b> 
+<input type="text" size="1" name="T20"/>Какую функцию выполняет команда <img height="20" width="20" src="images\41.jpg">?</b><br/>
+<input type="radio" name="Q20"/> равномерное заполнение объектов по заданной траектории или ее части;<br />
+<input type="radio" name="Q20"/> равномерное заполнение объектов в круговом массиве;<br />
+<input type="radio" name="Q20"/> равномерное заполнение объектов в прямоугольном массиве;<br />
+<input type="radio" name="Q20"/> построение сплайна.<br />
+<br/></li></ol>     
+<CENTER>
+<P><TEXTAREA name="s1" rows="4" cols="70" readonly> </TEXTAREA> </P>
+ 
+<INPUT onclick="check_me()" type="button" value="Показать результат"/>&nbsp;&nbsp;&nbsp;&nbsp; 
+<INPUT type="reset" value="Сбросить ответы"/> 
+</CENTER>      
+<br>
+<center><p style="font-family: Tahoma shadow 15px size 20pt"><a href="6.php">◄ Вернуться назад к изучению ►</a></p></center>    
+</form></p>
+     </div>
+      <p>&nbsp;</p></div></content>
+</body>
+</html>
---
+++ testreg.php
@@ -0,0 +1,84 @@
+
+<?php
+    session_start();//  вся процедура работает на сессиях. Именно в ней хранятся данные  пользователя, пока он находится на сайте. Очень важно запустить их в  самом начале странички!!!
+if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} }
+if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
+if (isset($_POST['mail'])) { $mail = $_POST['mail']; if ($mail == '') { unset($mail);} }
+if (isset($_POST['grup'])) { $grup=$_POST['grup']; if ($grup =='') { unset($grup);} }
+if (isset($_POST['fam'])) { $fam = $_POST['fam']; if ($fam == '') { unset($fam);} }
+if (isset($_POST['imja'])) { $imja=$_POST['imja']; if ($imja =='') { unset($imja);} }
+if (isset($_POST['otch'])) { $otch = $_POST['otch']; if ($otch == '') { unset($otch);} }
+?>
+<!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8" lang="ru">
+    <link rel="stylesheet" type="text/css" href="content\css\1.css">
+    <title>Главная страница</title>
+    </head>
+    <body>
+    
+    <div id="head"><p>Электронное учебное пособие по программе AutoCAD</p></div>
+
+    <div class="content1">
+
+<?php
+if (empty($login) && empty($password) && empty($mail) && empty($grup) && empty($fam) && empty($imja) && empty($otch)) //если пользователь не ввел значения, то выдаем ошибку и останавливаем скрипт
+    {
+    exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
+    }
+    //если логин и пароль введены,то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+    $login = stripslashes($login);
+    $login = htmlspecialchars($login);
+    $password = stripslashes($password);
+    $password = htmlspecialchars($password);
+    $mail = stripslashes($mail);
+    $mail = htmlspecialchars($mail);
+    $grup = stripslashes($grup);
+    $grup = htmlspecialchars($grup);
+    $fam = stripslashes($fam);
+    $fam = htmlspecialchars($fam);
+    $imja = stripslashes($imja);
+    $imja = htmlspecialchars($imja);
+    $otch = stripslashes($otch);
+    $otch = htmlspecialchars($otch);
+//удаляем лишние пробелы
+    $login = trim($login);
+    $password = trim($password);
+    $mail = trim($mail);
+    $password = trim($password);
+    $grup = trim($grup);
+    $fam = trim($fam);
+    $imja = trim($imja);
+    $otch = trim($otch);
+// подключаемся к базе
+    include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь 
+ 
+$result = mysql_query("SELECT * FROM users WHERE login='$login'",$db); //извлекаем из базы все данные о пользователе с введенным логином
+    $myrow = mysql_fetch_array($result);
+    if (empty($myrow['password']))
+    {
+    //если пользователя с введенным логином не существует
+    exit ("Извините, введённый вами login или пароль неверный.");
+    }
+    else {
+    //если существует, то сверяем пароли
+    if ($myrow['password']==$password) {
+    //если пароли совпадают, то запускаем пользователю сессию! Можете его поздравить, он вошел!
+    $_SESSION['login']=$myrow['login']; 
+    $_SESSION['id']=$myrow['id'];
+    $_SESSION['mail']=$myrow['mail'];
+    $_SESSION['grup']=$myrow['grup'];
+    $_SESSION['fam']=$myrow['fam'];
+    $_SESSION['imja']=$myrow['imja'];
+    $_SESSION['otch']=$myrow['otch'];
+    //эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
+    echo "Вы успешно вошли на сайт! <a href='index.php'>Главная страница</a>";
+    }
+ else {
+    //если пароли не сошлись
+
+    exit ("Извините, введённый вами login или пароль неверный.");
+    }
+    }
+    ?></div></body></html>
